783dd52e136297c1683c113230454efaaba5f370
Burkova Anastasiia <anastasiia.burkova@aalto.fi>
2022-05-10 21:21:19 +0300

    Adding configs for deployment

--- shopping-lists/app.js
+++ shopping-lists/app.js
@@ -11,8 +11,8 @@ configure({
 
 let port = 7777;
 if (Deno.args.length > 0) {
-const lastArgument = Deno.args[Deno.args.length - 1];
-port = Number(lastArgument);
+   const lastArgument = Deno.args[Deno.args.length - 1];
+   port = Number(lastArgument);
 };
 
 
